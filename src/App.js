071d4109f7c7d133e3059f408315b82a0e6f6ed5
Vinayak <vinayaksaubhri@gmail.com>
2021-01-17 22:38:53 +0530

    adding html form element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { FormControl,MenuItem,Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-     <h1>hello world</h1>
+      <div className="app__header">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="2">1</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+    
      {/* Header */}
      {/* Title + Select input dropdown field */}
 
